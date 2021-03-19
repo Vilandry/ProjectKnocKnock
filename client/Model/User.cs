c846069a7d130589a9112c366f047b5b309f3c06
theVilandry <adam0801k@gmail.com>
2021-03-19 10:07:43 +0100

    Add project files.

---
+++ client/Model/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum AGECATEGORY
+{
+    SIXTEEN,
+    TWENTY,
+    TWENTYFIVEPLUS
+}
+
+public enum SEX
+{
+    FEMALE,
+    MALE,
+    OTHER
+}
+
+namespace client.Model
+{
+    class User
+    {
+        private int userid;
+        private AGECATEGORY agecat;
+        private String username;
+
+        private List<int> friendIDList;
+    }
+}
